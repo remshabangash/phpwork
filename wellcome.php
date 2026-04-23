85ae19d78fbbf711fc9a92d622fc184c155ca655
REMSHA678 <bangashremsha0@gmail.com>
2026-04-23 14:53:35 +0500

    new

--- wellcome.php
+++ wellcome.php
@@ -7,14 +7,31 @@ E-mail: <input type="text" name="email"><br>
 Age: <input type="number" name="Age"><br>
 <input type="submit">
 </form>
+<table>
+    <tr><th>Name </th>
+<th>age</th>
+<th>Email</th></tr>
 
+<?php
+$con = mysqli_connect("localhost" , "root" , "" ,"studentdatabase");
+$result=mysqli_query($con , "SELECT * FROM profile");
+while($row=mysqli_fetch_assoc($result)){?>
+<tr>
+    <td><?php echo $row["Name"]?></td>
+    <td><?php echo $row["age"]?></td>
+    <td><?php echo $row["Email"]?></td>
+</tr>
+<?php }
+?>
+        
+    </table>
 </body>
 </html>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-$con = mysqli_connect("localhost" , "root" , "" ,"studentdatabase");
+
 if(!$con)die("connection failed : " .mysqli_connect_error());
 if ($_SERVER["REQUEST_METHOD"]=="POST")
     {
